c8ae5ab7ae4dda7749ff7c6c703063584e61b6b5
Jade Oliveira - JOI <jade.oliveira@iteris.com.br>
2021-08-09 18:14:55 -0300

    primeiro commit projeto contador de pessoas - flutter

---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //função que precisa para rodar a aplicação e é do tipo material app para suportar widgets do tipo material design
+  runApp(MaterialApp(
+      //parâmetros opcionais do materialApp
+      title: 'Contador de Pessoas',
+      //tela inicial do nosso app
+      home: Home()));
+}
+
+
+//criamos um widget que possui um stado interno que no caso é o _HomeState
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = 'Pode entrar!';
+
+  //função para modificar número de pessoas
+  void _changePeople(int delta) {
+    //uso o setState para informar o flutter que haverá uma mudança aqui
+    //chama o build para redesenhar a tela com a variável _people modificada nesse caso
+    //não refaz a tela inteira apenas as partes que foram modificadas
+    setState(() {
+      _people += delta;
+
+      if(_people < 0) {
+        _infoText = 'Mundo invertido?!';
+      } else if (_people <= 10) {
+        _infoText = 'Pode entrar!';
+      } else {
+        _infoText = 'Lotado!';
+      }
+    });
+  }
+
+  //a função build é chamada sempre que queremos modificar alguma coisa no layout
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/restaurant.jpg',
+          fit: BoxFit.cover,
+          height: 1000,
+        ),
+        Column(
+          //alinhamento do eixo principal
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pessoas: $_people',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: Text('+1',
+                          style: TextStyle(fontSize: 40, color: Colors.white))),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                      child: Text('-1',
+                          style: TextStyle(fontSize: 40, color: Colors.white))),
+                ),
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
