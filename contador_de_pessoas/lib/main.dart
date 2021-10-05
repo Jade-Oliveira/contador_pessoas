0d8fa7aedea3629c401df94023aaa93852644fc7
Jade Oliveira <jade.oliveira@iteris.com.br>
2021-10-05 10:55:59 -0300

    adicionando scaffold e editando textos

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   //função que precisa para rodar a aplicação e é do tipo material app para suportar widgets do tipo material design
   runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       //parâmetros opcionais do materialApp
       title: 'Contador de Pessoas',
       //tela inicial do nosso app
       home: Home()));
 }
-
 
 //criamos um widget que possui um stado interno que no caso é o _HomeState
 class Home extends StatefulWidget {
@@ -28,7 +28,7 @@ class _HomeState extends State<Home> {
     setState(() {
       _people += delta;
 
-      if(_people < 0) {
+      if (_people < 0) {
         _infoText = 'Mundo invertido?!';
       } else if (_people <= 10) {
         _infoText = 'Pode entrar!';
@@ -41,55 +41,62 @@ class _HomeState extends State<Home> {
   //a função build é chamada sempre que queremos modificar alguma coisa no layout
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset(
-          'images/restaurant.jpg',
-          fit: BoxFit.cover,
-          height: 1000,
-        ),
-        Column(
-          //alinhamento do eixo principal
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Pessoas: $_people',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextButton(
-                      onPressed: () {
-                        _changePeople(1);
-                      },
-                      child: Text('+1',
-                          style: TextStyle(fontSize: 40, color: Colors.white))),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextButton(
-                      onPressed: () {
-                        _changePeople(-1);
-                      },
-                      child: Text('-1',
-                          style: TextStyle(fontSize: 40, color: Colors.white))),
-                ),
-              ],
-            ),
-            Text(
-              _infoText,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30),
-            ),
-          ],
-        ),
-      ],
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'images/restaurant.jpg',
+            fit: BoxFit.cover,
+            height: 1000,
+          ),
+          Column(
+            //alinhamento do eixo principal
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Pessoas: $_people',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextButton(
+                        onPressed: () {
+                          _changePeople(1);
+                        },
+                        child: Text('+1',
+                            style:
+                                TextStyle(fontSize: 48, color: Colors.white))),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextButton(
+                        onPressed: () {
+                          _changePeople(-1);
+                        },
+                        child: Text('-1',
+                            style:
+                                TextStyle(fontSize: 48, color: Colors.white))),
+                  ),
+                ],
+              ),
+              Text(
+                _infoText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
